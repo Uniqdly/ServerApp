6a05b0faf5dbb69c3702b0273f00171b1039e9d4
Uniqdly <yurych2015@mail.ru>
2024-05-18 16:55:06 +0500

    Добавил комментарии

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
 
 class InfoController extends Controller
 {
@@ -14,15 +13,21 @@ class InfoController extends Controller
         phpinfo();  // Вызываем phpinfo()
         $phpinfo = ob_get_clean(); // Получаем содержимое буфера и очищаем его
 
-        // Возвращаем содержимое phpinfo() в виде JSON
-        return response()->json(['phpinfo' => $phpinfo]);
+        return response($phpinfo, 200)
+                ->header('Content-Type', 'text/html'); // Устанавливаем заголовок Content-Type
     }
 
     // Метод для отображения информации о клиенте
+        // Метод для отображения информации о клиенте
     public function clientInfo(Request $request)
     {
+        // Получаем IP-адрес клиента из запроса
         $ip = $request->ip();
+        
+        // Получаем User-Agent клиента из заголовков запроса
         $userAgent = $request->header('User-Agent');
+        
+        // Возвращаем данные о клиенте в формате JSON
         return response()->json(['ip' => $ip, 'userAgent' => $userAgent]);
     }
 
@@ -30,12 +35,18 @@ class InfoController extends Controller
     public function databaseInfo()
     {
         try {
+            // Пытаемся получить соединение с базой данных
             $connection = DB::connection()->getPdo();
+            
+            // Получаем имя базы данных
             $dbName = DB::connection()->getDatabaseName();
+            
+            // Возвращаем информацию о базе данных в формате JSON
             return response()->json(['database' => $dbName, 'connection' => $connection->getAttribute(\PDO::ATTR_CONNECTION_STATUS)]);
         } catch (\Exception $e) {
+            // Если возникла ошибка при подключении к базе данных, возвращаем сообщение об ошибке в формате JSON
             return response()->json(['error' => 'Could not connect to the database.'], 500);
         }
     }
-}
 
+}
